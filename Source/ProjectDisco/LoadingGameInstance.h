68419600eaffd1f0062004b9e0f1e032bdecf695
Thompson-HenryJ <hjt014@student.usc.edu.au>
2025-09-16 12:21:27 +1000

    C++ Configuration
    Implementation of C++ source files and start of building for loading screen functionality

---
+++ Source/ProjectDisco/LoadingGameInstance.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "LoadingGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDISCO_API ULoadingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Init() override;
+
+	UFUNCTION()
+	virtual void BeginLoadingScreen(const FString& MapName);
+
+	UFUNCTION()
+	virtual void EndLoadingScreen(UWorld* InLoadedWorld);
+
+};
