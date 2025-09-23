caa010c7b8b18c8d933d828df49290a32f5ba8b8
Thompson-HenryJ <hjt014@student.usc.edu.au>
2025-09-23 11:28:39 +1000

    Reapply "File pathway correction for Module creation (LoadingScreenModule)"
    This reverts commit bb9c9f862272e7adcd55b2d5227bd756e20c62ad.

--- Source/LoadingScreenModule/LoadingScreenModule.Build.cs
+++ Source/LoadingScreenModule/LoadingScreenModule.Build.cs
@@ -13,12 +13,20 @@ using UnrealBuildTool;
             //Use shared precompiled headers for better compile times
             PCHUsage = PCHUsageMode.UseSharedPCHs;
 
+            PublicDependencyModuleNames.AddRange(
+                new string[] {
+                    "Core",
+                    "Modules"
+                }
+            );
+
+
             //Add core unreal engine modules as private dependencies
             PrivateDependencyModuleNames.AddRange(
                 new string[] {
                     "Core",         //Core UE functionality
-                    "CoreUObject",   //Base UObject system
-                    
+                    "CoreUObject"   //Base UObject system
+                   
                 }
             );
         }
--- Source/LoadingScreenModule/Private/LoadingScreenModule.cpp
+++ Source/LoadingScreenModule/Private/LoadingScreenModule.cpp
@@ -21,4 +21,4 @@ void FLoadingScreenModule::StartLoadingScreen()
 }
 
 //Registers this module withg Unreal Engine's Module system
-IMPLEMENT_GAME_MODULE(FLoadingScreenModule, LoadingScreenModule);
+void IMPLEMENT_GAME_MODULE(FLoadingScreenModule, LoadingScreenModule);
