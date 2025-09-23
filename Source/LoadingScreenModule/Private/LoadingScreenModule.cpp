452d2f2fca27489cfc3dae7ad6884225a0ca3631
Thompson-HenryJ <hjt014@student.usc.edu.au>
2025-09-23 11:23:26 +1000

    Reapply "Start of a loading screen module setup"
    This reverts commit d4c6ec693b6336b91c309da643949cf8dff92868.

---
+++ Source/LoadingScreenModule/Private/LoadingScreenModule.cpp
@@ -0,0 +1,24 @@
+#include "../Public/LoadingScreenModule.h"
+
+void FLoadingScreenModule::StartupModule()
+{
+	//Log when module initialises
+	UE_LOG(LogTemp, Warning, TEXT("FloadingScreenModule::StartupModule"));
+
+}
+
+bool FLoadingScreenModule::IsGameModule() const
+{
+	//Identifies this as a runtime module vs editor-only
+	return true;
+}
+
+void FLoadingScreenModule::StartLoadingScreen()
+{
+	//Log when loading screen is requested
+	UE_LOG(LogTemp, Warning, TEXT("FLoadingScreenModule::StartLoadingScreen"));
+
+}
+
+//Registers this module withg Unreal Engine's Module system
+IMPLEMENT_GAME_MODULE(FLoadingScreenModule, LoadingScreenModule);
